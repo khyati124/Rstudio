dc8a4010f29754b5f61c332f8d35ddb69ab6a669
khyatisonavane <khyatisonavane@gmail.com>
2020-08-16 19:14:48 +0530

    Functions of R

---
+++ Functions.R
@@ -0,0 +1,23 @@
+rnorm(n=5, mean=10, sd=8)
+
+c()
+seq(10, 20, length.out = 3)
+x <- c("a", "b", "c")
+c()
+A <- seq(10, 20, along.with = x)
+rep()
+rep(x, times=5)
+
+print()
+
+is.numeric()
+is.double()
+is.character()
+typeof()
+
+A
+B <- sqrt(A)
+B
+paste()
+
+#?
